5528a6455e69c69f2c5a7de0215835446566e6e7
Divyanshi Gupta <71611825+realdivs@users.noreply.github.com>
2025-05-17 23:43:46 +0530

    Update theme-context.js

--- src/theme-context.js
+++ src/theme-context.js
@@ -1,1 +1,24 @@
 
+import { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
+
+export const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prevState) => !prevState);
+  };
+
+  const theme = isDarkMode ? "dark" : "light";
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
