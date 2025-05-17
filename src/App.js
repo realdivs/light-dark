ed55eecdc4d4caefad62036a4e22a45e4d989f84
Divyanshi Gupta <71611825+realdivs@users.noreply.github.com>
2025-05-17 23:44:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./theme-context";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
+    <ThemeProvider>
+      <BrowserRouter>
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blog" element={<Blog />} />
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
